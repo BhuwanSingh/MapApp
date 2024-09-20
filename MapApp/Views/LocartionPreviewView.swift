f976f14755a0486022b7ee0ce30a8988a007adb2
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2024-09-20 15:30:15 +0530

    working on location detail

---
+++ MapApp/Views/LocartionPreviewView.swift
@@ -0,0 +1,18 @@
+//
+//  LocartionPreviewView.swift
+//  MapApp
+//
+//  Created by Bhuwan Singh on 20/09/24.
+//
+
+import SwiftUI
+
+struct LocartionPreviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocartionPreviewView()
+}
