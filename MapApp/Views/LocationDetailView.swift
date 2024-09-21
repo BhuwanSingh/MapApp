08c246198c755a0ff132fde404d3b05a12f22fb7
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2024-09-21 16:49:47 +0530

    App complete

--- MapApp/Views/LocationDetailView.swift
+++ MapApp/Views/LocationDetailView.swift
@@ -42,7 +42,8 @@ extension LocationDetailView {
                 Image($0)
                     .resizable()
                     .scaledToFill()
-                    .frame(width: UIScreen.main.bounds.width)
+                    .frame(width: UIDevice.current.userInterfaceIdiom == .pad ? nil :
+                            UIScreen.main.bounds.width)
                     .clipped()
             }
         }
--- MapApp/Views/LocationsView.swift
+++ MapApp/Views/LocationsView.swift
@@ -10,6 +10,8 @@ import MapKit
 
 struct LocationsView: View {
     @EnvironmentObject private var viewModel: LocationsViewModel
+    let maxWidthForIpad: CGFloat = 700
+    
     var body: some View {
         ZStack{
             mapLayer
@@ -18,6 +20,7 @@ struct LocationsView: View {
             VStack (spacing: 0){
                 header
                     .padding()
+                    .frame(maxWidth: maxWidthForIpad)
                 Spacer()
                 locaitonsPreviewStack
             }
@@ -79,6 +82,8 @@ extension LocationsView {
                     LocartionPreviewView(location: location)
                         .shadow(color: Color.black.opacity(0.3), radius: 20)
                         .padding()
+                        .frame(maxWidth: maxWidthForIpad)
+                        .frame(maxWidth: .infinity)
                         .transition( .asymmetric(
                             insertion: .move(edge: .trailing),
                             removal: .move(edge: .leading)))
